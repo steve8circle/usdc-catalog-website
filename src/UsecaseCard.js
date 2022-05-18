55edaaa40583cce28655043184a0f0a1267eb4c4
Steve Jin <steve.jin@circle.com>
2022-05-17 20:23:14 -0700

    extract header and footer components, add use case logo sample

--- src/UsecaseCard.js
+++ src/UsecaseCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import bookIcon from "./book-icon.png";
 
 export default function UsecaseCard({ name, description, tags }) {
   return (
@@ -9,11 +10,10 @@ export default function UsecaseCard({ name, description, tags }) {
           to={`/detail/${name}`}
           class="flex flex-wrap no-underline hover:no-underline mt-5"
         >
-          <img
-            class="h-12 w-12 mt-2"
-            src="https://cdn-icons.flaticon.com/png/128/2838/premium/2838912.png?token=exp=1652328464~hmac=dbf27736ae4df5e285f7bf1acde9bbe3"
-          />
-          <div class="font-bold text-xl text-gray-800 px-6 mt-4">{name}</div>
+          <div class="flex items-center">  
+            <img class="h-10 w-12 ml-4" src={bookIcon} /> 
+            <div class="font-bold text-xl text-gray-800 px-6">{name}</div>
+          </div>
         </Link>
         <div class="text-gray-800 text-sm px-6 mb-5 text-left">
           {description}
